efab8bd10a9552e4037f2ee0a664ebfcdb65d9d7
Yurgish <humenyuriipetrovych@gmail.com>
2024-10-28 16:46:46 +0200

    fixed: display bugs

--- src/sidebar/components/Settings/General/SettingItems/DefaultModelSetting/index.tsx
+++ src/sidebar/components/Settings/General/SettingItems/DefaultModelSetting/index.tsx
@@ -1,4 +1,5 @@
-import { cn } from '@nextui-org/react'
+import { Icon } from '@iconify/react/dist/iconify.js'
+import { Button, cn } from '@nextui-org/react'
 import { useLiveQuery } from 'dexie-react-hooks'
 import { useTranslation } from 'react-i18next'
 
@@ -29,22 +30,25 @@ const DefaultModelSetting = () => {
       className={cn(
         'flex max-w-2xl overflow-hidden rounded-lg border-2 border-transparent bg-content1 shadow-md',
         'select-none rounded-lg p-4 hover:bg-content2',
-        defaultModel && 'border-2 border-foreground'
+        defaultModel?.value && 'border-2 border-foreground'
       )}
     >
       <div className="inline-flex w-full flex-row flex-wrap items-center justify-between gap-2">
         <div className="flex flex-col gap-1">
           <p className="text-medium">{t('settings.general.defaultModel.label')}</p>
-          <p className="text-tiny text-default-400">
+          <p className="max-w-64 text-tiny text-default-400">
             {t('settings.general.defaultModel.description')}
           </p>
         </div>
-        <div className="flex min-w-40 justify-end">
+        <div className="flex items-center gap-2">
           <SelectModelDropdown
             selectedModel={defaultModel?.value ? getDefaultModel(defaultModel?.value) : undefined}
             models={models}
             onSelectModel={handleSelectDefaultModel}
           />
+          <Button isIconOnly size="sm" onClick={() => handleSelectDefaultModel('')}>
+            <Icon icon="radix-icons:reset" width={20} />
+          </Button>
         </div>
       </div>
     </div>
